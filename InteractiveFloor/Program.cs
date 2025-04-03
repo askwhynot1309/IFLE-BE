9b29c76152326e1cc5f1312d8b7ddebe2cafd809
AskWhyNot <tommarvoloriddle1975@gmail.com>
2025-04-03 18:05:14 +0700

    add game basic crud with game version and category

--- InteractiveFloor/Program.cs
+++ InteractiveFloor/Program.cs
@@ -2,6 +2,7 @@ using DAO;
 using Microsoft.EntityFrameworkCore;
 using Repository;
 using Service;
+using InteractiveFloor.Mappings;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,8 +16,15 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<InteractiveFloorManagementContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
+
+// repos
 builder.Services.AddScoped<IGameCategoryRepository, GameCategoryRepository>();
+builder.Services.AddScoped<IGameRepository, GameRepository>();
+
+// services
 builder.Services.AddScoped<IGameCategoryService, GameCategoryService>();
+builder.Services.AddScoped<IGameService, GameService>();
 
 var app = builder.Build();
 
