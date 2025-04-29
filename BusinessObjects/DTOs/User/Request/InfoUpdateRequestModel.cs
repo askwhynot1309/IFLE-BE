85b37693c0918d18a1adb19c34ec5ed60d837dd2
PhuHoang07 <duongphu1207@gmail.com>
2025-04-30 04:18:35 +0700

    Revert "[fix]:"
    This reverts commit 129aec9f3a94ba39cd182190a5bdc38b42e08000.

--- BusinessObjects/DTOs/User/Request/InfoUpdateRequestModel.cs
+++ BusinessObjects/DTOs/User/Request/InfoUpdateRequestModel.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +8,6 @@ namespace BusinessObjects.DTOs.User.Request
 {
     public class InfoUpdateRequestModel
     {
-        [Required(ErrorMessage = "Vui lòng không để trống tên người dùng.")]
         public string FullName { get; set; } = null!;
 
     }
