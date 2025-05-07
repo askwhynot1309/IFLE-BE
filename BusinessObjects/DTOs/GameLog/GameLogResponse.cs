c6479d2e6d8cca31d01ec1733639724c5213b336
AskWhyNot <tommarvoloriddle1975@gmail.com>
2025-05-07 21:05:52 +0700

    add game log create, view all, view by game

---
+++ BusinessObjects/DTOs/GameLog/GameLogResponse.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace BusinessObjects.DTOs.GameLog
+{
+    public class GameLogResponse
+    {
+        public string Id { get; set; } = null!;
+        public DateTime UpdateTime { get; set; }
+        public string Description { get; set; } = null!;
+        public string UpdateStatus { get; set; } = null!;
+        public string StaffId { get; set; } = null!;
+        public string GameId { get; set; } = null!;
+        public string StaffName { get; set; } = null!;
+        public string GameTitle { get; set; } = null!;
+    }
+} 
